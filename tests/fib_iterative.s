85c6cd8a8024fdb600f48518040cfceb05bf05f0
workfunction <jason840507@gmail.com>
2018-01-18 21:47:37 +0800

    Implement fibonacci intrative, recursive and tail recursive

---
+++ tests/fib_iterative.s
@@ -0,0 +1,26 @@
+; init terms to calculate in #3
+print "Term to calculate:"
+scan #3
+
+; input = 0
+add $0 $0 #2
+jz #3 :output
+
+; f0 = 0, f1 = 1
+add $0 $0 #1
+add $1 $0 #2
+
+; input = 1
+sub #3 $1 #3
+jz #3 :output
+
+fib:
+add #1 #2 #2
+sub #2 #1 #1
+
+sub #3 $1 #3
+jnz #3 :fib
+
+output:
+print #2
+halt
